5de559f88dcb9cca31b829527c7fc88ac1121800
kornel841 <94543223+kornel841@users.noreply.github.com>
2023-07-21 18:43:30 +0200

    Update run.py

--- run.py
+++ run.py
@@ -3,7 +3,6 @@ import json
 import requests
 from bs4 import BeautifulSoup
 from dotenv import load_dotenv
-from tqdm import tqdm
 import time
 
 # Load environment variables from .env file
@@ -12,8 +11,12 @@ load_dotenv()
 class QuoteScraper:
     BASE_URL = os.environ.get("INPUT_URL")
     OUTPUT_FILE = os.environ.get("OUTPUT_FILE")
-    PROXY = os.environ.get("PROXY")
-    HEADERS = {"User-Agent": "Mozilla/5.0"}
+    PROXY = os.environ.get(
+    PROXY = os
+"PROXY")
+    HEADERS = {
+    HEADERS
+"User-Agent": "Mozilla/5.0"}
 
     def __init__(self):
         self.session = self._create_session()
@@ -21,50 +24,117 @@ class QuoteScraper:
 
     def _create_session(self):
         session = requests.Session()
-        if self.PROXY:
+        
+       
+if self.PROXY:
             session.proxies = {"http": self.PROXY, "https": self.PROXY}
-        return session
+        
+       
+return session
 
-    def _fetch_quotes(self, url):
+    
+
+   
+def _fetch_quotes(self, url):
         try:
             response = self.session.get(url, headers=self.HEADERS)
             response.raise_for_status()
             return response.text
         except requests.exceptions.RequestException as e:
-            print(f"Failed to fetch {url}: {e}")
+            
+           
+print(f"Failed to fetch {url}: {e}")
             return None
 
     def _scrape_quotes(self, page_content):
-        soup = BeautifulSoup(page_content, "html.parser")
+        soup = BeautifulSoup(page_content, 
+        soup
+"html.parser")
         for quote in soup.select(".quote"):
             text = quote.select_one(".text").text
-            author = quote.select_one(".author").text
+            author = quote.select_one(
+            author = quote.select
+".author").text
             tags = [tag.text for tag in quote.select(".tag")]
             self.quotes.append({"text": text, "by": author, "tags": tags})
 
     def _save_quotes_to_jsonl(self):
         with open(self.OUTPUT_FILE, "w") as f:
-            for quote in self.quotes:
-                f.write(json.dumps(quote) + "\n")
+            
+           
+for quote in self.quotes:
+                f.write(json.dumps(quote) + 
+                f.write
 
-    def scrape_quotes(self):
-        page_number = 1
-        while True:
-            url = f"{self.BASE_URL}page/{page_number}/"
+               
+"\n")
+
+    
+
+   
+
+
+def scrape_quotes(self):
+        page_number = 
+       
+1
+        
+       
+while True:
+            url = 
+            url = f
+
+            url
+
+           
+f"{self.BASE_URL}page/{page_number}/"
             page_content = self._fetch_quotes(url)
-            if page_content is None:
+            
+            page_content = self._fetch_quotes(url)
+           
+
+            page_content = self._fetch_quotes(url)
+
+           
+if page_content is None:
                 break
             self._scrape_quotes(page_content)
-            page_number += 1
-            time.sleep(3)  # Add a delay to respect website's policy (3 seconds here, adjust if necessary)
+            page_number += 
+            self._sc
+
+           
+1
+            time.sleep(
+            time.sleep(
+
+           
+3)  # Add a delay to respect website's policy (3 seconds here, adjust if necessary)
 
         self._save_quotes_to_jsonl()
 
+
 def main():
     scraper = QuoteScraper()
-    print("Scraping quotes...")
+    
+    scraper = QuoteScraper()
+   
+
+    scraper = QuoteScraper()
+
+   
+print("Scraping quotes...")
     scraper.scrape_quotes()
-    print("Quotes scraped successfully!")
+    
+    scraper.scrape_quotes()
+   
+
+    scraper.scrape_quotes()
+
+   
+print("Quotes scraped successfully!")
 
 if __name__ == "__main__":
     main()
+
+    main()
+``
